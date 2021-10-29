1cfa987eef2ab958cb8ac69df905128ea4c3c41e
Abinaya2k <abinayaanbuselvann@gmail.com>
2021-10-29 11:11:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import Counter from './components/Counter';
+import Header from './components/Header'
+import Auth from './components/Auth'
+import UserProfile from './components/UserProfile'
+function App() {
+  const isauth=useSelector(state=>state.auth.isAuth)
+  return (
+    <Fragment>
+      <Header/>
+      {!isauth && <Auth/>}
+      {isauth && <UserProfile/>}
+      <Counter />
+    </Fragment>
+    
+  );
+}
+
+export default App;
